b6906bd81e9a4ddacdffee21d9092512a3b8e211
Akram495 <akrammiah457@gmail.com>
2025-02-06 23:39:40 +0000

    Fix NLTK LookupError by adding nltk.download

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+import os
+
+# Set the NLTK data path manually
+nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
+nltk.data.path.append(nltk_data_path)
+
+# Download necessary NLTK resources
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
+
 
 import streamlit as st
 import pickle
